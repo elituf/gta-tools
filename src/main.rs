a84401ca33e6b26a3f7a9e9c7d9e68e0c42e82fe
futile <git@futile.eu>
2025-04-30 00:48:44 +0100

    fail compilation if compiling outside of windows

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[cfg(not(target_os = "windows"))]
+compile_error!("This application must be compiled on Windows.");
+
 mod features;
 mod gui;
 mod util;
