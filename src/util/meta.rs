0a831580e281e07f94e8fe30fc5c51c041c067c2
futile <git@futile.eu>
2025-05-16 10:10:16 +0100

    use reworked logging to log errors in `util::meta::default`

--- src/util/meta.rs
+++ src/util/meta.rs
@@ -1,4 +1,4 @@
-use crate::util::codeberg;
+use crate::{util::codeberg, util::log};
 use semver::Version;
 
 #[derive(Debug)]
@@ -11,7 +11,14 @@ pub struct Meta {
 impl Default for Meta {
     fn default() -> Self {
         let current_version = Version::parse(env!("CARGO_PKG_VERSION")).unwrap();
-        let latest_release = codeberg::get_latest_release().unwrap_or_default();
+        let latest_release = match codeberg::get_latest_release() {
+            Ok(release) => release,
+            Err(why) => {
+                let message = format!("failed to get latest codeberg release:\n{why}");
+                log::log(log::LogLevel::Error, message);
+                codeberg::Release::default()
+            }
+        };
         let newer_version_available = matches!(
             &current_version.cmp_precedence(&latest_release.version),
             std::cmp::Ordering::Less
