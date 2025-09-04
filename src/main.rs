81b4b26f96504b548f4bc1cdfa430aae9cb7f969
futile <git@futile.eu>
2025-09-04 09:44:07 +0100

    small grammar fix on non-windows compilation error

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 #[cfg(not(target_os = "windows"))]
-compile_error!("This application must be compiled on Windows.");
+compile_error!("This application must be compiled for Windows.");
 
 mod features;
 mod gui;
