114292d912bb92363b216358171522168b27e0c9
futile <git@futile.eu>
2025-11-04 22:56:03 +0000

    use `tauri-winres` instead of `winresource` & specify some more fields in the windows resource in `build.rs`

--- build.rs
+++ build.rs
@@ -1,6 +1,10 @@
 fn main() {
     static_vcruntime::metabuild();
-    winresource::WindowsResource::new()
+    tauri_winres::WindowsResource::new()
+        .set("FileDescription", "GTA Tools")
+        .set("ProductName", "GTA Tools")
+        .set("LegalCopyright", "futile <git@futile.eu>")
+        .set_language(0x0009)
         .set_icon("assets/icon.ico")
         .compile()
         .unwrap();
