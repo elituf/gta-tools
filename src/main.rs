09e19b6e160b6d575161b93bf78360685fc8de2e
futile <git@futile.eu>
2025-11-06 05:06:57 +0000

    remove hardly useful "windows compilation only" warning

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[cfg(not(target_os = "windows"))]
-compile_error!("This application must be compiled for Windows.");
-
 mod features;
 mod gui;
 mod util;
